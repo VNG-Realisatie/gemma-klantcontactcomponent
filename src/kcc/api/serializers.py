69aae3cdc1a1f87908b6de4e4854a29d76a8ea2b
annashamray <anna.shamray@maykinmedia.nl>
2019-10-18 17:57:36 +0200

    create serializers for new models

--- src/kcc/api/serializers.py
+++ src/kcc/api/serializers.py
@@ -1,12 +1,127 @@
 import logging
 
 from rest_framework import serializers
+from vng_api_common.serializers import add_choice_values_help_text
 
-from kcc.datamodel.models import ContactMoment, Klant
+from kcc.datamodel.constants import GeslachtsAanduiding
+from kcc.datamodel.models import (
+    Adres,
+    ContactMoment,
+    Klant,
+    NatuurlijkPersoon,
+    SubVerblijfBuitenland,
+    Vestiging,
+)
 
 logger = logging.getLogger(__name__)
 
 
+# klant models
+class SubVerblijfBuitenlandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubVerblijfBuitenland
+        fields = (
+            "lnd_landcode",
+            "lnd_landnaam",
+            "sub_adres_buitenland_1",
+            "sub_adres_buitenland_2",
+            "sub_adres_buitenland_3",
+        )
+
+
+class VerblijfsAdresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Adres
+        fields = (
+            "aoa_identificatie",
+            "wpl_woonplaats_naam",
+            "gor_openbare_ruimte_naam",
+            "aoa_postcode",
+            "aoa_huisnummer",
+            "aoa_huisletter",
+            "aoa_huisnummertoevoeging",
+            "inp_locatiebeschrijving",
+        )
+
+
+class NatuurlijkPersoonSerializer(serializers.ModelSerializer):
+    verblijfsadres = VerblijfsAdresSerializer(required=False, allow_null=True)
+    sub_verblijf_buitenland = SubVerblijfBuitenlandSerializer(
+        required=False, allow_null=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        value_display_mapping = add_choice_values_help_text(GeslachtsAanduiding)
+        self.fields["geslachtsaanduiding"].help_text += f"\n\n{value_display_mapping}"
+
+    class Meta:
+        model = NatuurlijkPersoon
+        fields = (
+            "inp_bsn",
+            "anp_identificatie",
+            "inp_a_nummer",
+            "geslachtsnaam",
+            "voorvoegsel_geslachtsnaam",
+            "voorletters",
+            "voornamen",
+            "geslachtsaanduiding",
+            "geboortedatum",
+            "verblijfsadres",
+            "sub_verblijf_buitenland",
+        )
+
+    def create(self, validated_data):
+        verblijfsadres_data = validated_data.pop("verblijfsadres", None)
+        sub_verblijf_buitenland_data = validated_data.pop(
+            "sub_verblijf_buitenland", None
+        )
+        natuurlijkpersoon = super().create(validated_data)
+
+        if verblijfsadres_data:
+            verblijfsadres_data["natuurlijkpersoon"] = natuurlijkpersoon
+            VerblijfsAdresSerializer().create(verblijfsadres_data)
+
+        if sub_verblijf_buitenland_data:
+            sub_verblijf_buitenland_data["natuurlijkpersoon"] = natuurlijkpersoon
+            SubVerblijfBuitenlandSerializer().create(sub_verblijf_buitenland_data)
+        return natuurlijkpersoon
+
+
+class RolVestigingSerializer(serializers.ModelSerializer):
+    verblijfsadres = VerblijfsAdresSerializer(required=False, allow_null=True)
+    sub_verblijf_buitenland = SubVerblijfBuitenlandSerializer(
+        required=False, allow_null=True
+    )
+
+    class Meta:
+        model = Vestiging
+        fields = (
+            "vestigings_nummer",
+            "handelsnaam",
+            "verblijfsadres",
+            "sub_verblijf_buitenland",
+        )
+
+    def create(self, validated_data):
+        verblijfsadres_data = validated_data.pop("verblijfsadres", None)
+        sub_verblijf_buitenland_data = validated_data.pop(
+            "sub_verblijf_buitenland", None
+        )
+        vestiging = super().create(validated_data)
+
+        if verblijfsadres_data:
+            verblijfsadres_data["vestiging"] = vestiging
+            VerblijfsAdresSerializer().create(verblijfsadres_data)
+
+        if sub_verblijf_buitenland_data:
+            sub_verblijf_buitenland_data["vestiging"] = vestiging
+            SubVerblijfBuitenlandSerializer().create(sub_verblijf_buitenland_data)
+        return vestiging
+
+
+# main models
 class KlantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Klant
