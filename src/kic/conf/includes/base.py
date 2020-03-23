d784559a9e547b28a1fec65d74ca7b629ce26d4d
Joeri Bekker <joeri@maykinmedia.nl>
2020-03-23 16:26:25 +0100

    :wrench: Added includes

---
+++ src/kic/conf/includes/base.py
@@ -0,0 +1,378 @@
+import os
+
+import raven
+
+from .api import *  # noqa
+from .environ import config
+
+
+# Build paths inside the project, so further paths can be defined relative to
+# the code root.
+DJANGO_PROJECT_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)
+)
+BASE_DIR = os.path.abspath(
+    os.path.join(DJANGO_PROJECT_DIR, os.path.pardir, os.path.pardir)
+)
+
+#
+# Core Django settings
+#
+SITE_ID = config("SITE_ID", default=1)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY")
+
+# NEVER run with DEBUG=True in production-like environments
+DEBUG = config("DEBUG", default=False)
+
+# = domains we're running on
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", split=True)
+
+IS_HTTPS = config("IS_HTTPS", default=not DEBUG)
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = "nl-nl"
+
+TIME_ZONE = "UTC"  # note: this *may* affect the output of DRF datetimes
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+USE_THOUSAND_SEPARATOR = True
+
+#
+# DATABASE and CACHING setup
+#
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": config("DB_NAME", "zrc"),
+        "USER": config("DB_USER", "zrc"),
+        "PASSWORD": config("DB_PASSWORD", "zrc"),
+        "HOST": config("DB_HOST", "localhost"),
+        "PORT": config("DB_PORT", 5432),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{config('CACHE_DEFAULT', 'localhost:6379/0')}",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
+        },
+    },
+    "axes": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{config('CACHE_AXES', 'localhost:6379/0')}",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
+        },
+    },
+    "drc_sync": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{config('CACHE_DEFAULT', 'localhost:6379/0')}",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
+        },
+    },
+}
+
+# Application definition
+
+INSTALLED_APPS = [
+    # Note: contenttypes should be first, see Django ticket #10827
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.sessions",
+    # Note: If enabled, at least one Site object is required
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Optional applications.
+    "django.contrib.admin",
+    "django.contrib.gis",
+    # 'django.contrib.admindocs',
+    # 'django.contrib.humanize',
+    # External applications.
+    "axes",
+    "django_filters",
+    "corsheaders",
+    "vng_api_common",  # before drf_yasg to override the management command
+    "vng_api_common.authorizations",
+    "vng_api_common.audittrails",
+    "vng_api_common.notifications",
+    "drf_yasg",
+    "rest_framework",
+    "rest_framework_gis",
+    "django_markup",
+    "solo",
+    # Project applications.
+    "zrc.accounts",
+    "zrc.api",
+    "zrc.datamodel",
+    "zrc.sync",
+    "zrc.utils",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "vng_api_common.middleware.AuthMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "vng_api_common.middleware.APIVersionHeaderMiddleware",
+]
+
+ROOT_URLCONF = "zrc.urls"
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+)
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(DJANGO_PROJECT_DIR, "templates")],
+        "APP_DIRS": False,  # conflicts with explicity specifying the loaders
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "zrc.utils.context_processors.settings",
+            ],
+            "loaders": TEMPLATE_LOADERS,
+        },
+    }
+]
+
+WSGI_APPLICATION = "zrc.wsgi.application"
+
+# Translations
+LOCALE_PATHS = (os.path.join(DJANGO_PROJECT_DIR, "conf", "locale"),)
+
+#
+# SERVING of static and media files
+#
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(DJANGO_PROJECT_DIR, "static"),
+    os.path.join(BASE_DIR, "node_modules", "font-awesome"),
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "/media/"
+
+#
+# Sending EMAIL
+#
+EMAIL_HOST = config("EMAIL_HOST", default="localhost")
+EMAIL_PORT = config(
+    "EMAIL_PORT", default=25
+)  # disabled on Google Cloud, use 487 instead
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False)
+EMAIL_TIMEOUT = 10
+
+DEFAULT_FROM_EMAIL = "zrc@example.com"
+
+FIXTURE_DIRS = (os.path.join(DJANGO_PROJECT_DIR, "fixtures"),)
+
+#
+# LOGGING
+#
+LOGGING_DIR = os.path.join(BASE_DIR, "log")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s %(levelname)s %(name)s %(module)s %(process)d %(thread)d  %(message)s"
+        },
+        "timestamped": {"format": "%(asctime)s %(levelname)s %(name)s  %(message)s"},
+        "simple": {"format": "%(levelname)s  %(message)s"},
+        "performance": {"format": "%(asctime)s %(process)d | %(thread)d | %(message)s"},
+    },
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "timestamped",
+        },
+        "django": {
+            "level": "DEBUG",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": os.path.join(LOGGING_DIR, "django.log"),
+            "formatter": "verbose",
+            "maxBytes": 1024 * 1024 * 10,  # 10 MB
+            "backupCount": 10,
+        },
+        "project": {
+            "level": "DEBUG",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": os.path.join(LOGGING_DIR, "zrc.log"),
+            "formatter": "verbose",
+            "maxBytes": 1024 * 1024 * 10,  # 10 MB
+            "backupCount": 10,
+        },
+        "performance": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": os.path.join(LOGGING_DIR, "performance.log"),
+            "formatter": "performance",
+            "maxBytes": 1024 * 1024 * 10,  # 10 MB
+            "backupCount": 10,
+        },
+    },
+    "loggers": {
+        "zrc": {"handlers": ["project"], "level": "INFO", "propagate": True},
+        "django.request": {"handlers": ["django"], "level": "ERROR", "propagate": True},
+        "django.template": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
+
+#
+# AUTH settings - user accounts, passwords, backends...
+#
+AUTH_USER_MODEL = "accounts.User"
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+# Allow logging in with both username+password and email+password
+AUTHENTICATION_BACKENDS = [
+    "zrc.accounts.backends.UserModelEmailBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
+SESSION_COOKIE_NAME = "zrc_sessionid"
+
+#
+# SECURITY settings
+#
+SESSION_COOKIE_SECURE = IS_HTTPS
+SESSION_COOKIE_HTTPONLY = True
+
+CSRF_COOKIE_SECURE = IS_HTTPS
+
+X_FRAME_OPTIONS = "DENY"
+
+#
+# Silenced checks
+#
+SILENCED_SYSTEM_CHECKS = ["rest_framework.W001"]
+
+#
+# Custom settings
+#
+PROJECT_NAME = "Zaken"
+SITE_TITLE = "Zaak Registratie Component (ZRC)"
+
+ENVIRONMENT = None
+SHOW_ALERT = True
+
+#
+# Library settings
+#
+
+# Django-axes
+AXES_CACHE = "axes"  # refers to CACHES setting
+AXES_LOGIN_FAILURE_LIMIT = 30  # Default: 3
+AXES_LOCK_OUT_AT_FAILURE = True  # Default: True
+AXES_USE_USER_AGENT = False  # Default: False
+AXES_COOLOFF_TIME = 1  # One hour
+AXES_BEHIND_REVERSE_PROXY = IS_HTTPS  # We have either Ingress or Nginx
+AXES_ONLY_USER_FAILURES = (
+    False  # Default: False (you might want to block on username rather than IP)
+)
+AXES_LOCK_OUT_BY_COMBINATION_USER_AND_IP = (
+    False  # Default: False (you might want to block on username and IP)
+)
+
+#
+# DJANGO-CORS-MIDDLEWARE
+#
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = (
+    "x-requested-with",
+    "content-type",
+    "accept",
+    "origin",
+    "authorization",
+    "x-csrftoken",
+    "user-agent",
+    "accept-encoding",
+    "accept-crs",
+    "content-crs",
+)
+
+if "GIT_SHA" in os.environ:
+    GIT_SHA = config("GIT_SHA")
+else:
+    GIT_SHA = raven.fetch_git_sha(BASE_DIR)
+
+# Raven
+SENTRY_DSN = config("SENTRY_DSN", None)
+
+if SENTRY_DSN:
+    INSTALLED_APPS = INSTALLED_APPS + ["raven.contrib.django.raven_compat"]
+
+    RAVEN_CONFIG = {"dsn": SENTRY_DSN, "release": GIT_SHA}
+    LOGGING["handlers"].update(
+        {
+            "sentry": {
+                "level": "WARNING",
+                "class": "raven.handlers.logging.SentryHandler",
+                "dsn": RAVEN_CONFIG["dsn"],
+            }
+        }
+    )
